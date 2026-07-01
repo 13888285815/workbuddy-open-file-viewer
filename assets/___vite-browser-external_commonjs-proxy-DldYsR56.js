823aa83dba673f46c3123fadd67f7e1371902f3a
zzx <zzx@users.noreply.github.com>
2026-07-01 17:23:10 +0800

    v1.1.6: Fix base path - use VITE_BASE_PATH env, dev=/ gh-pages=/workbuddy-open-file-viewer/

---
+++ assets/___vite-browser-external_commonjs-proxy-DldYsR56.js
@@ -0,0 +1,1 @@
+import{r as e}from"./open-file-viewer-Bq1ZpeRg.js";const r={},t=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"})),a=e(t);export{a as r};
--- assets/___vite-browser-external_commonjs-proxy-VhHnu2Sp.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./open-file-viewer-HaVlj_XH.js";const r={},t=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"})),a=e(t);export{a as r};
